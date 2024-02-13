f99a7153eb3fb0d4289e3f20ec43707e05e796b5
Ying Wang <yingw787.personal@gmail.com>
2024-02-13 18:33:43 -0500

    Update schema.ts update Track model and add Module model

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -19,6 +19,12 @@ export const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
   "Author of a complete Track"
@@ -28,5 +34,13 @@ export const typeDefs = gql`
     name: String!
     "Author's profile picture url"
     photo: String
+  }
+
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 `;
