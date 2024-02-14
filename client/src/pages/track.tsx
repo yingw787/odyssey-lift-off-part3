72727f60c55d00721429418340586b076dae46d7
Ying Wang <yingw787.personal@gmail.com>
2024-02-14 12:24:55 -0500

    Update track.tsx update gql query

--- client/src/pages/track.tsx
+++ client/src/pages/track.tsx
@@ -5,9 +5,33 @@ import { useParams } from "react-router-dom";
 import { gql } from "../__generated__";
 import { Layout, QueryResult } from "../components";
 
+export const GET_TRACK = gql(`
+query GetTrack($trackId: ID!) {
+  track(id: $trackId) {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+    numberOfViews
+    modules {
+      id
+      title
+      length
+    }
+    description
+  }
+}
+`);
+
 const Track = () => {
   const { trackId = "" } = useParams();
-  return <Layout></Layout>
-}
+  return <Layout></Layout>;
+};
 
 export default Track;
